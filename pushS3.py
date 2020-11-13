6b292e549ece17e9dfac972fe084e24f37a5679b
emmett <surftastic@hotmail.com>
2020-11-13 16:56:34 +0000

    working pushS3.py

--- pushS3.py
+++ pushS3.py
@@ -1,2 +1,11 @@
 import boto3
 
+def pushfile():
+    s3_resource = boto3.resource('s3')
+    s3_resource.Bucket('genesis-bton').upload_file(
+        Filename='toast.txt', Key='toast.txt')
+
+    print('Got here')
+
+if __name__ == '__main__':
+    pushfile()
